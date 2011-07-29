0d85c3ffb249f4a4c9ff93fdd5867bfdb8e95b58
Jamie Chen <jachen@ucsd.edu>
2011-07-29 14:00:51 -0700

    Update ioncore to 0.4.44

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setupdict['test_suite'] = 'epuagent'
 #setupdict['include_package_data'] = True
 #setupdict['package_data'] = {
 #    'epu': ['data/*.sqlt', 'data/install.sh']
-setupdict['install_requires'] = ['ioncore==0.4.43']
+setupdict['install_requires'] = ['ioncore==0.4.44']
 
 setup(**setupdict)
