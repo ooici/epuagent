e6235425817d1d182c1ee5201fa2c4e7909936a1
Jamie Chen <jachen@ucsd.edu>
2011-10-19 09:15:19 -0700

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 
 setupdict = {
     'name' : 'epuagent',
-    'version' : '0.4.3',
+    'version' : '1.0.0',
     'description' : 'OOICI CEI Elastic Processing Unit Agents',
     'url': 'https://confluence.oceanobservatories.org/display/CIDev/Common+Execution+Infrastructure+Development',
     'download_url' : 'http://ooici.net/packages',
